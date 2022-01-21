f4fa54a4612ba0319b75a208eaeab9d0138be59b
unknown <andreinichol.e.castro.dev@gmail.com>
2022-01-21 16:37:17 +0800

    implemented the email system on schedule confirm/cancel for admin manage schedule

--- admin/update-package.php
+++ admin/update-package.php
@@ -205,9 +205,7 @@ foreach($results as $result)
 
 </div>
 <!--inner block end here-->
-<!--copy rights start here-->
-<?php include('includes/footer.php');?>
-<!--COPY rights end here-->
+
 </div>
 </div>
   <!--//content-inner-->
