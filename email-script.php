d481228da0da458e8a4d20809a3a01f627801427
unknown <andreinichol.e.castro.dev@gmail.com>
2022-01-21 15:39:18 +0800

    email system is now working on the inquiry page

---
+++ email-script.php
@@ -0,0 +1,13 @@
+<?php
+
+if (isset($_POST['submit1'])) {
+    $fullName = $_POST["fname"];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['description'];
+    $mailTo = 'krazergran2@gmail.com';
+    $headers  = "From: " . $mailFrom;
+    $txt = "Email from " . $fullName . ". \n\n" . $message;
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: thankyou-email.php");
+}
